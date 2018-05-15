8c02a12a8d8cfce685507a63f0fe2e7cb9a8237c
NVisionLabs <aliwer406@gmail.com>
2018-05-15 05:11:31 +0100

    Login with token

--- resources/assets/js/components/login/Login.vue
+++ resources/assets/js/components/login/Login.vue
@@ -1,11 +1,50 @@
 <template>
- <h1>Login</h1> 
+    <v-container>  
+  <v-form @submit.prevent="login">
+
+    <v-text-field
+      label="E-mail"
+      v-model="form.email"
+      type="email"
+      required
+    ></v-text-field>
+
+
+    <v-text-field
+      label="Password"  
+      v-model="form.password"
+      type="password"
+      required
+    ></v-text-field>
+
+    <v-btn
+    color="green"
+    type="submit"
+    >Login</v-btn>
+
+  </v-form>
+ </v-container>  
+
 </template>
 
 
 <script>
 export default {
-
+    data(){
+        return {
+            form :{
+                email:null,
+                password:null
+            }
+        }
+    }, 
+    methods:{
+        login(){
+           axious.post('/api/auth/login',this.form)
+            .then(res => console.log(res.data))
+            .catch(error => console.log(error.response.data)) 
+        }
+    }
 }
 </script>
 
